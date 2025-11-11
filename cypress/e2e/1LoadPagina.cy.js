b2031fb977dff6f67c2e10b2dbb75a4efd81ca79
baujuncos <127853844+baujuncos@users.noreply.github.com>
2025-11-10 23:57:05 -0300

    Add files via upload

--- cypress/e2e/1LoadPagina.cy.js
+++ cypress/e2e/1LoadPagina.cy.js
@@ -1,6 +1,6 @@
    describe('Prueba de carga de página', () => {
    it('Carga correctamente la página', () => {
-     cy.visit('/') // Colocar la url local o de Azure de nuestro front: https://webapp-tp05-qa-juncos-treachi-fqa5gug9addretfg.canadacentral-01.azurewebsites.net
+     cy.visit('https://webapp-tp05-qa-juncos-treachi-fqa5gug9addretfg.canadacentral-01.azurewebsites.net') // Colocar la url local o de Azure de nuestro front: https://webapp-tp05-qa-juncos-treachi-fqa5gug9addretfg.canadacentral-01.azurewebsites.net
      cy.get('h1').should('contain', 'TikTask') // Verifica que el título contenga "TikTask"
    })
  })
--- cypress/e2e/2Login.cy.js
+++ cypress/e2e/2Login.cy.js
@@ -1,6 +1,6 @@
    describe('Prueba de Login', () => {
    it('El login es exitoso', () => {
-     cy.visit('/') // Colocar la url local o de Azure de nuestro front
+     cy.visit('https://webapp-tp05-qa-juncos-treachi-fqa5gug9addretfg.canadacentral-01.azurewebsites.net') // Colocar la url local o de Azure de nuestro front
      cy.get('h1').should('contain', 'TikTask') // Verifica que el título contenga "TikTask"
      cy.get('#loginUsername').click();
      cy.get('#loginUsername').type('admin');
